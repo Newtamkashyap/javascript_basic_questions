4a45df03c30eb3d8a3f6508a649fea36d56a04e1
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2025-04-11 12:19:35 +0530

    naturalNumber

--- natural_number_exam.js
+++ natural_number_exam.js
@@ -1,11 +1,12 @@
 function sumNaturalNumbers(n) {
     let sum = 0;
     for (let i = 1; i <= n; i++) {
+      console.log(i)
       sum += i;
     }
     return sum;
   }
   
-  const n = 10; 
-  console.log(`sum of the natural number ${n} is : ${sumNaturalNumbers(n)}`);
+  // const n = 10; 
+  console.log(`sum of the natural number  is : ${sumNaturalNumbers(5)}`);
   
