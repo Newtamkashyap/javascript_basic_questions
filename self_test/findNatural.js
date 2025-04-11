cc9766d5d14d3e01825f5ae9fa80b3e4d1acbb65
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2025-04-11 12:20:40 +0530

    Finding_NaturalNumber

---
+++ self_test/findNatural.js
@@ -0,0 +1,12 @@
+function get_sumOf_NaturalNumbers(){
+    var sumOfAll=0;
+    for(var i=1;i<=100;i++){
+        console.log(`${i}`);
+
+       
+    }
+    return sumOfAll+=i;
+
+    
+}
+console.log(get_sumOf_NaturalNumbers());
