9b03e038f39b612935ce016899fb363ea84d6c10
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2025-04-11 08:57:22 +0530

    odd-number

---
+++ self_test/findOdd.js
@@ -0,0 +1,11 @@
+function findOddNumber(num){
+    if(num%2!==0){
+        console.log("Odd number")
+
+    }
+    else{
+        console.log("not odd number")
+    }
+}
+findOddNumber(8);
+findOddNumber(3);
